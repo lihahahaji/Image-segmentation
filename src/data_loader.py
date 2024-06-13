4fc426f5ebbddb04b39b31922343f7ad1cc9739e
Haji <835510106@qq.com>
2024-06-13 16:17:07 +0800

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -23,10 +23,13 @@ class SynapseDataset(Dataset):
             data = np.load(file_path)
             image = data['image']
             label = data['label']
+            # print("image_size:{} , label_size:{}".format(image.shape,label.shape))
+
         elif file_path.endswith('.h5'):
             with h5py.File(file_path, 'r') as f:
                 image = np.array(f['image'])
                 label = np.array(f['label'])
+                # print("image_size:{} , label_size:{}".format(image.shape,label.shape))
         else:
             raise ValueError("Unsupported file format")
         
@@ -34,8 +37,12 @@ class SynapseDataset(Dataset):
             image, label = self.transform(image, label)
         
         # Convert to torch tensors
-        image = torch.tensor(image, dtype=torch.float32).unsqueeze(0)  # Add channel dimension
-        label = torch.tensor(label, dtype=torch.long)
+        if file_path.endswith('.h5'):
+            label = torch.tensor(label, dtype=torch.long)
+        else:
+            image = torch.tensor(image, dtype=torch.float32).unsqueeze(0)  # Add channel dimension
+            label = torch.tensor(label, dtype=torch.long)
+
         
         return image, label
 
@@ -45,11 +52,22 @@ def load_data_list(file_path):
     return data_list
 
 # Example usage
-# train_list = load_data_list('data_set/Synapse/lists/lists_Synapse/train.txt')
-# test_list = load_data_list('data_set/Synapse/lists/lists_Synapse/test_vol.txt')
 
-# train_dataset = SynapseDataset(train_list, 'data_set/Synapse/train_npz')
-# test_dataset = SynapseDataset(test_list, 'data_set/Synapse/test_vol_h5')
 
-# train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True)
-# test_loader = DataLoader(test_dataset, batch_size=8, shuffle=False)
+
+train_list = load_data_list('data_set/Synapse/lists/lists_Synapse/train.txt')
+test_list = load_data_list('data_set/Synapse/lists/lists_Synapse/test_vol.txt')
+
+train_dataset = SynapseDataset(train_list, 'data_set/Synapse/train_npz')
+test_dataset = SynapseDataset(test_list, 'data_set/Synapse/test_vol_h5')
+
+print(test_dataset)
+
+train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+
+for images,labels in test_loader:
+    print("test_loader",images.shape,labels.shape)
+
+for images,labels in train_loader:
+    print("train_loader",images.shape,labels.shape)
